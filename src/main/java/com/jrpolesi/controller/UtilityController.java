79efc4f89f6ebe4dc90af160e6f5f3c37b0de5fa
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2025-06-08 13:04:25 -0300

    Add exercise 1.2

--- src/main/java/com/jrpolesi/controller/UtilityController.java
+++ src/main/java/com/jrpolesi/controller/UtilityController.java
@@ -3,6 +3,10 @@ package com.jrpolesi.controller;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
 public class UtilityController implements IController {
     private final Javalin app;
 
@@ -13,10 +17,19 @@ public class UtilityController implements IController {
     @Override
     public void initRoutes() {
         app.get("/hello", this::helloWorld);
+        app.get("/status", this::getStatus);
     }
 
     private void helloWorld(Context ctx) {
         ctx.contentType("text/plain; charset=UTF-8");
         ctx.result("Hello, Javalin!");
+    }
+
+    private void getStatus(Context ctx) {
+        final var timestamp = ZonedDateTime.now().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+        ctx.json(Map.of(
+                "status", "ok",
+                "timestamp", timestamp
+        ));
     }
 }
