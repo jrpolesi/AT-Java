9cf842e9edb5fee96c931bba4d8f6a855b0cc68e
jose ricardo kremer polesi <josericardo.kremer@gmail.com>
2025-06-08 17:42:05 -0300

    Add exercise 2.3

--- src/test/java/com/jrpolesi/controller/ToDoControllerTest.java
+++ src/test/java/com/jrpolesi/controller/ToDoControllerTest.java
@@ -13,6 +13,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ToDoControllerTest {
+    private static final String CONTENT_TYPE = "application/json";
+    private static final String DEFAULT_TITULO = "Fazer AT Java";
+    private static final String DEFAULT_DESCRICAO = "Implementar testes para API REST";
+    private static final boolean DEFAULT_CONCLUIDO = false;
+
     private final ObjectMapper objectMapper = new ObjectMapper();
     private Javalin app;
 
@@ -25,36 +30,70 @@ class ToDoControllerTest {
         toDoController.initRoutes();
     }
 
-    private void assertToDoResponse(String response, String titulo, String descricao, boolean concluido) throws Exception {
-        var responseJson = objectMapper.readValue(response, ToDoResponseDTO.class);
-
-        assertNotNull(responseJson.id());
-        assertNotNull(responseJson.dataCriacao());
-        assertEquals(titulo, responseJson.titulo());
-        assertEquals(descricao, responseJson.descricao());
-        assertEquals(concluido, responseJson.concluido());
-    }
-
     @Test
-    void givenValidBody_whenCallsPOSTToDo_shouldReturn201() throws Exception {
+    void givenValidBody_whenCallsPOSTToDo_thenShouldReturn201() {
         JavalinTest.test(app, (server, client) -> {
-            var titulo = "Fazer AT Java";
-            var descricao = "Implementar testes para API REST";
-            var concluido = false;
-
+            // given
             var todoRequest = """
                     {
                         "titulo": "%s",
                         "descricao": "%s",
                         "concluido": %b
                     }
-                    """.formatted(titulo, descricao, concluido);
+                    """.formatted(DEFAULT_TITULO, DEFAULT_DESCRICAO, DEFAULT_CONCLUIDO);
 
+            // when
             var response = client.post("/todo", todoRequest);
+            var responseBody = objectMapper.readValue(response.body().string(), ToDoResponseDTO.class);
 
+            // then
             assertEquals(201, response.code());
-            assertEquals("application/json", response.header("Content-Type"));
-            assertToDoResponse(response.body().string(), titulo, descricao, concluido);
+            assertEquals(CONTENT_TYPE, response.header("Content-Type"));
+            assertToDo(responseBody);
         });
+    }
+
+    @Test
+    void givenExistingToDo_whenCallsGETToDoById_ThenShouldReturn200() {
+        JavalinTest.test(app, (server, client) -> {
+            // given
+            var todoRequest = """
+                    {
+                        "titulo": "%s",
+                        "descricao": "%s",
+                        "concluido": %b
+                    }
+                    """.formatted(DEFAULT_TITULO, DEFAULT_DESCRICAO, DEFAULT_CONCLUIDO);
+
+            var createResponse = client.post("/todo", todoRequest);
+            var createdToDo = objectMapper.readValue(createResponse.body().string(), ToDoResponseDTO.class);
+
+            // when
+            var getResponse = client.get("/todo/" + createdToDo.id());
+            var responseBody = objectMapper.readValue(getResponse.body().string(), ToDoResponseDTO.class);
+
+            // then
+            assertEquals(200, getResponse.code());
+            assertEquals(CONTENT_TYPE, getResponse.header("Content-Type"));
+            assertToDoEquals(createdToDo, responseBody);
+        });
+    }
+
+    private void assertToDo(ToDoResponseDTO todo) {
+        assertNotNull(todo);
+        assertNotNull(todo.id());
+        assertNotNull(todo.dataCriacao());
+        assertEquals(DEFAULT_TITULO, todo.titulo());
+        assertEquals(DEFAULT_DESCRICAO, todo.descricao());
+        assertEquals(DEFAULT_CONCLUIDO, todo.concluido());
+    }
+
+    private void assertToDoEquals(ToDoResponseDTO expected, ToDoResponseDTO actual) {
+        assertNotNull(actual);
+        assertEquals(expected.id(), actual.id());
+        assertEquals(expected.dataCriacao(), actual.dataCriacao());
+        assertEquals(expected.titulo(), actual.titulo());
+        assertEquals(expected.descricao(), actual.descricao());
+        assertEquals(expected.concluido(), actual.concluido());
     }
 } 
